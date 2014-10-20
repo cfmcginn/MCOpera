478d7dc7248bfa32ef0a549703df994240897762
Christopher McGinn <cfmcginn@Christophers-Mac-mini.local>
2014-10-20 22:12:51 +0200

    Working seeded

--- prunOperaMC.sh
+++ prunOperaMC.sh
@@ -10,6 +10,6 @@ now="mcOperaJob_$(date +"%m_%d_%Y__%H_%M_%S")"
 mkdir $now
 
 NAME="runOperaMC.cpp"
-g++ $NAME -Werror -Wall -O2 -o "${NAME/%.cpp/}.exe" -I/usr/X11R6/include -L/usr/X11R6/lib -lX11 -I/Users/cfmcginn/CPPLibraries/boost_1_56_0/
+g++ $NAME $(root-config --cflags --libs) -Werror -Wall -O2 -o "${NAME/%.cpp/}.exe" -I/usr/X11R6/include -L/usr/X11R6/lib -lX11 -I/Users/cfmcginn/CPPLibraries/boost_1_56_0/
 
 mv *.exe $now
--- runOperaMC.cpp
+++ runOperaMC.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <time.h>
+
+#include "TFile.h"
+#include "TH1F.h"
 
 #include "boost/random/mersenne_twister.hpp"
 #include "boost/random/uniform_int_distribution.hpp"
@@ -9,12 +13,30 @@ boost::random::mt19937 gen;
 
 int main(int argc, char*argv[])
 {
-  if(argc!=2){
-    std::cout << "Usage: runOperaMC <fracSize>" << std::endl;
+  if(argc!=4){
+    std::cout << "Usage: runOperaMC <fracSize> <nEvts> <outName>" << std::endl;
     return 1;
   }
 
-  std::cout << "Fraction: " << getRandomFraction(atoi(argv[1])) << std::endl;
+  gen.seed(static_cast<unsigned int>(std::time(0)));
+
+  TH1F* testHist_p = new TH1F("testHist_h", "testHist_h", 10, -0.05, .95);
+
+  for(int iter = 0; iter < atoi(argv[2]); iter++){
+    float randNum = getRandomFraction(atoi(argv[1]));
+    testHist_p->Fill(randNum);
+  }
+
+  TFile* outFile_p = new TFile(Form("%s.root", argv[3]), "UPDATE");
+  TH1::SetDefaultSumw2();
+  testHist_p->Scale(1./testHist_p->Integral());
+  testHist_p->SetMaximum(.1999);
+  testHist_p->SetMinimum(.0001);
+  testHist_p->Write("", TObject::kOverwrite);
+  outFile_p->Close();
+  delete outFile_p;
+
+  delete testHist_p;
 
   return 0;
 }
@@ -23,6 +45,6 @@ float getRandomFraction(int fracSize)
 {
   boost::random::uniform_int_distribution<> dist(0, fracSize);
   float fracNum = dist(gen);
-  return fracNum/((float)fracSize);
+  return fracNum/((float)(fracSize+1));
 }
 
